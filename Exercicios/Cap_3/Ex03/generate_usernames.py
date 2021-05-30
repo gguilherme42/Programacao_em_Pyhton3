8103b302458dd0f6c57ba72e7713a19f5a19d995
gguilherme42 <58306122+gguilherme42@users.noreply.github.com>
2021-05-29 23:06:50 -0300

    Ex03 done

--- Exercicios/Cap_3/Ex03/generate_usernames.py
+++ Exercicios/Cap_3/Ex03/generate_usernames.py
@@ -11,7 +11,7 @@ def validate_start() -> None:
         sys.exit()
 
 
-def generate_username(fields: list(), usernames: set) -> str:
+def generate_username(fields: list, usernames: set) -> str:
     new_username = ((fields[FORENAME][0] + fields[MIDDLENAME][:1] + fields[SURNAME]).replace("-", "").replace("'", ""))
     new_username = original_username = new_username.lower()[:8] # limit of an username is 8
     count = 1
@@ -32,26 +32,57 @@ def process_line(line: str, usernames: set) -> User_tuple:
 
 
 
-def print_header(name_width: int, username_width: int) -> None:
-    print(f"{'Name':<{name_width}} {'ID':^6} {'Username':{username_width}}")
-    print(f"{'':-<{name_width}} {'':-<6} {'':-<{username_width}}")
+def printable_header(name_width: int, username_width: int) -> str:
+    return f"{'Name':<{name_width}} {'ID':^6} {'Username':{username_width}}"
+    
+
+def printable_divider(name_width: int, username_width: int) -> str:
+    return f"{'':-<{name_width}} {'':-<6} {'':-<{username_width}}"
 
 
+def fix_len_columns(column1_list: list, column2_list: list) -> None:
+    if len(column1_list) > len(column2_list):
+        column2_list.append("")
+    elif len(column1_list) < len(column2_list):
+        column1_list.append("")
 
-def print_users(users: dict) -> None:
-    name_width = 32
-    username_width = 9
-    print_header(name_width, username_width)
 
+def two_users_per_line(column1_list: list, column2_list: list, users: dict, name_width: int, username_width: int) -> None:
+    count = 0
     for key in sorted(users):
         user = users[key]
         initial = ""
         if user.middlename:
             initial = " " + user.middlename[0]
         name = f"{user.surname}, {user.forename} {initial}"
+        name_limit = name_width if len(name) >= name_width else len(name)
+        name = name[:name_limit]
         
-        print(f"{name:.<{name_width}} ({user.id:4}) {user.username:{username_width}}")
+        printable_result = f"{name:.<{name_width}} ({user.id:4}) {user.username:{username_width}}"
 
+        if count % 2 == 0:
+            column1_list.append(printable_result)
+        else:
+            column2_list.append(printable_result)
+        count += 1
+    
+    fix_len_columns(column1_list, column2_list)
+
+
+
+
+def print_users(users: dict) -> None:
+    name_width = 17
+    username_width = 9
+    
+
+    column1_list = [printable_header(name_width, username_width), printable_divider(name_width, username_width)]
+    column2_list = [printable_header(name_width, username_width), printable_divider(name_width, username_width)]
+
+    two_users_per_line(column1_list, column2_list, users, name_width, username_width)
+
+    for column1, column2 in zip(column1_list, column2_list):
+        print(f"{column1}    {column2}")
 
 
 def main() -> None:
