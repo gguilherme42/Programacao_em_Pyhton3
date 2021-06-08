0217c19c55dc1a7c7bd50a80e9e4620afe577234
gguilherme42 <58306122+gguilherme42@users.noreply.github.com>
2021-06-07 21:46:13 -0300

    Add function return_lst_files

--- Exercicios/Cap_4/Ex01/ex01.py
+++ Exercicios/Cap_4/Ex01/ex01.py
@@ -2,9 +2,37 @@ import os
 import sys
 
 
-print(os.listdir('./'))
+def return_lst_files() -> list[str]:
+    existing_files = os.listdir('.')
+   
+
+    filtered_files = filter(lambda file: file.endswith('.lst'), existing_files)
+
+    return [*filtered_files]
+
 
 def user_option() -> str:
     option = input('[A]dd [D]elete [S]ave [Q]uit [a]: ').lower().strip()[0]
     return option
+
+
+def open_file(file_name: str) -> None:
+    fh = None
+    try:
+        fh = open(file_name, encoding='utf8')
+    except EnvironmentError as err:
+        print(f'ERROR: {err}')
+    
+
+
+
+
+
+def main(): 
+    # file = input('Choose file name: ').strip()
+    print(return_lst_files())
+
+
+main()
+
 
