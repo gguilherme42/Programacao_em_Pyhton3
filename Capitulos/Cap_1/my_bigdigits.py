a2e420a3d7631c787aff88b4894b2928a111a26c
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-03 22:53:43 -0300

    Corrigir bug: invalid literal for int() with base 10

--- Capitulos/Cap_1/my_bigdigits.py
+++ Capitulos/Cap_1/my_bigdigits.py
@@ -36,4 +36,35 @@ seven = []
 
 eight = []
 
-nine = []
+nine = [" ****",
+        "*   *",
+        "*   *",
+        " ****",
+        "    *",
+        "    *",
+        "    *",]
+
+digits = [zero, one, two, three, four, five,
+        six, seven, eight, nine]
+
+
+try:
+    input_digits = sys.argv[1]
+    print(f"----> {sys.argv[1]}")
+    row = 0
+    while row < 7:
+        line = ""
+        column = 0
+        while column < len(input_digits):
+            number = int(input_digits[column])
+            input_digits = digits[number]
+            line += input_digits[row] + ""
+            column += 1
+        print(line)
+        row += 1
+
+except IndexError:
+    print("Usage: my_bigdigits.py <number>")
+
+except ValueError as err:
+    print(f"{err} in {input_digits}")
