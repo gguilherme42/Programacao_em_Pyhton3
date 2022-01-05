cb74fb337260ef774b6f4bee9e801d84cbe30235
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-05 10:13:49 -0300

    Fix exercise error

--- Capitulos/Cap_1/my_bigdigits.py
+++ Capitulos/Cap_1/my_bigdigits.py
@@ -48,20 +48,21 @@ digits = [zero, one, two, three, four, five,
         six, seven, eight, nine]
 
 
+
 try:
     input_digits = sys.argv[1]
-    print(f"----> {sys.argv[1]}")
-    row = 0
-    while row < 7:
+    print(f"----> {len(sys.argv[1])}")
+    
+    for row in range(7):
         line = ""
-        column = 0
-        while column < len(input_digits):
+
+        for column in range(len(input_digits)):  
             number = int(input_digits[column])
-            input_digits = digits[number]
-            line += input_digits[row] + ""
-            column += 1
+            selected_digit = digits[number]
+            line += selected_digit[row] + "  "
+        
         print(line)
-        row += 1
+    
 
 except IndexError:
     print("Usage: my_bigdigits.py <number>")
